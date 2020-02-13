50e2530425890034b458c9d9f79cccea918a69a9
jassim <jabdullatheef@stcs.com.sa>
2020-02-14 00:27:37 +0300

    fixed proxy

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientId,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
