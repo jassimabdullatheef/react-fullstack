cfa2430c034cdb6fc1526c1ed679d8ba1d9d1b27
jassim <jabdullatheef@stcs.com.sa>
2020-02-13 23:46:10 +0300

    added google auth with passport js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # react-fullstack
+
+add config/keys.js with following code
+```
+module.exports = {
+    googleClientId: 'client-id-from-google-developer-console',
+    googleClientSecret: 'client-secret-from-google-developer-console'
+}
+```
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+require('./models/User')
+require('./services/passport')
 
-app.get('/', (req, res) => {
-    res.send({'hi': 'there'})
-});
+mongoose.connect(keys.mongoURI)
+
+const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT);
+app.listen(PORT)
